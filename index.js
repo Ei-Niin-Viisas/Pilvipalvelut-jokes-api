2cceeb97b133e0285b9473869ed2bc94226ff74b
Ei-Niin-Viisas <102646430+Ei-Niin-Viisas@users.noreply.github.com>
2024-11-08 09:35:04 +0000

    lisätty /count

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
   res.send('Try /random_joke, /random_ten, /jokes/random, or /jokes/ten , /jokes/random/"any-number" \n Muokkasin tätä vähän :)');
 });
 
+app.get('/', (req, res) => {
+  res.send({"jokes":count})
+});
+
 app.get('/ping', (req, res) => {
   res.send('pong');
 });
